104f5c73ef40b4a2a6e666f32a59c4813c3b1465
Carolina <165325794+linacarol@users.noreply.github.com>
2024-05-18 17:25:13 -0300

    Update passagem.py

--- passagem.py
+++ passagem.py
@@ -1,15 +1,16 @@
 # RA 252062
 
-import sys
-
-# classe que guarda o dia, o mês e o ano de determinado voo
 class Data:
+    """
+    classe que guarda o dia, o mes e o ano de determinado voo
+
+    ela eh iniciada com os seguintes valores nesta ordem: dia, mes, ano
+    """
     
-    def __init__ (self, dia, mes, ano, voo='') :
+    def __init__ (self, dia, mes, ano) :
         self.dia = dia
         self.mes = mes
         self.ano = ano
-        self.voo = voo
     
     @property
     def dia(self) :
@@ -23,10 +24,6 @@ class Data:
     def ano(self) :
         return self._ano
     
-    @property
-    def voo(self) :
-        return self._voo
-    
     @dia.setter
     def dia(self, data) :
         self._dia = data
@@ -39,20 +36,18 @@ class Data:
     def ano(self, data) :
         self._ano = data
 
-    @voo.setter
-    def voo(self, valor) :
-        self._voo = valor
-
-# classe que guarda as informações dadas de determinado voo
 class Voo:
+    """"
+    classe que guarda as informacoes de determinado voo
 
-    def __init__ (self, numero, origem='', destino='', data='', valor='', indice='') :
-        self.numero = numero
+    ela eh iniciada com os seguintes valores nesta ordem: origem, destino, data, valor
+    """
+
+    def __init__ (self, origem, destino, data, valor) :
         self.origem = origem
         self.destino = destino
         self.data = data
         self.valor = valor
-        self.indice = indice
     
     @property
     def origem(self) :
@@ -70,10 +65,6 @@ class Voo:
     def valor(self) :
         return self._valor
     
-    @property
-    def indice(self) :
-        return self._indice
-    
     @origem.setter
     def origem(self, codigo) :
         self._origem = codigo
@@ -89,13 +80,13 @@ class Voo:
     @valor.setter
     def valor(self, preco) :
         self._valor = preco
-    
-    @indice.setter
-    def indice(self, i) :
-        self._indice = i
 
-# classe que guarda os dados de Jairzinho
 class Dados :
+    """"
+    classe que guarda os dados de Jairzinho
+
+    ela eh iniciada com os seguintes valores nesta ordem: saida, inicio das ferias, fim das ferias
+    """
 
     def __init__ (self, saida, inicio='', fim='') :
         self.saida = saida
@@ -126,18 +117,23 @@ class Dados :
     def fim(self, data) :
         self._fim = data
 
-# função que retorna a quantidade de dias em determinado mês
-# como os anos na entrada são sempre 2021 ou 2022, não são bissextos
 def dias_mes(mes) :
+    """"
+    funcao que retorna a quantidade de dias existentes em determinado mes
+    """
+
     if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12 :
         return 31
     if mes == 2 :
-        return 28
+        return 28       # como os anos da entrada sao sempre 2021 ou 2022, nunca eh bissexto
     if mes == 4 or mes == 6 or mes == 9 or mes == 11 :
         return 30
 
-# função que conta a duração em dias de uma data a outra
 def conta_dias(inicio, fim) :
+    """
+    funcao que recebe duas datas e retorna a quantidade de dias existentes entre elas
+    """
+    
     duracao = 0
     if inicio.ano == fim.ano :
         for i in range(inicio.mes+1, fim.mes) :
@@ -153,93 +149,85 @@ def conta_dias(inicio, fim) :
         duracao += fim.dia
     return duracao
 
-voos = []
-i = 0
+voos = dict()
+op = input()
 
-# entrada com cada uma das operações
-# lê o arquivo até End Of File
-for line in sys.stdin:
-    op = line.strip()
+# loop que recebe a entrada do usuário até que ele digite 'planejar'
+while op != 'planejar' :
     if op == 'registrar' :
-        voos.append(Voo(int(input())))
-        voos[i].origem, voos[i].destino = map(str, input().split())
-        voos[i].data = input()
-        voos[i].valor = float(input())
-        i += 1
+        num = int(input())
+        origem, destino = map(str, input().split())
+        data = input()
+        valor = float(input())
+        voos[num] = Voo(origem, destino, data, valor)       # os voos sao armazenados em dicionarios cujas chaves sao os numeros
     
     elif op == 'alterar' :
         num, novo_valor = map(str, input().split())
         num = int(num)
         novo_valor = float(novo_valor)
-        for j in voos :
-            if j.numero == num :
-                print (f'{num} valor alterado de {j.valor} para {novo_valor}')
-                j.valor = novo_valor
+        print (f'{num} valor alterado de {voos[num].valor} para {novo_valor}')
+        voos[num].valor = novo_valor
     
     elif op == 'cancelar' :
         num = int(input())
-        for j in voos :
-            if j.numero == num :
-                voos.remove(j)
+        del voos[num]
     
-    elif op == 'planejar' :
-        plano = Dados(input())
-        plano.inicio, plano.fim = map(str, input().split())
+    op = input()
 
-# definição das datas das férias e dos vôos
+# ao escolher 'planejar', Jairzinho insere seus dados
+plano = Dados(input())
+plano.inicio, plano.fim = map(str, input().split())
+
+# definicao das datas das ferias e dos voos
 comeca = Data(int(plano.inicio[:2]), int(plano.inicio[3:5]), int(plano.inicio[6:]))
 acaba = Data(int(plano.fim[:2]), int(plano.fim[3:5]), int(plano.fim[6:]))
-datas = []
-for i in range(len(voos)) :
-    datas.append(Data(int(voos[i].data[:2]), int(voos[i].data[3:5]), int(voos[i].data[6:]), voos[i].numero))
+datas = dict()
+for chave in voos.keys() :
+    datas[chave] = Data(int(voos[chave].data[:2]), int(voos[chave].data[3:5]), int(voos[chave].data[6:]))
 
-# criação de listas que contêm as possibilidades de idas e de voltas
+# criacao de listas que contem as possibilidades de idas e de voltas
 possiveis_idas = []
 possiveis_voltas = []
 
-# se a data do vôo está dentro do período de férias de Jairzinho e se a origem do vôo é o mesmo aeroporto de onde Jairzinho sai,
-# adicionamos esse vôo na lista de possíveis idas
-for i in range(len(voos)) :
-    if datas[i].ano == comeca.ano and voos[i].origem == plano.saida :
-        if (datas[i].mes == comeca.mes and datas[i].dia >= comeca.dia) or (datas[i].mes > comeca.mes) :
-            if ((datas[i].ano == acaba.ano and (datas[i].mes < acaba.mes or (datas[i].mes == acaba.mes and datas[i].dia < acaba.dia)))
-                or datas[i].ano < acaba.ano) :
-                voos[i].indice = i
-                possiveis_idas.append(voos[i])
-    elif datas[i].ano < comeca.ano and voos[i].origem == plano.saida :
-        voos[i].indice = i
-        possiveis_idas.append(voos[i])
+# se a data do voo esta dentro do periodo de ferias de Jairzinho e se a origem do voo eh o mesmo aeroporto de onde Jairzinho sai,
+# adicionamos esse voo na lista de possiveis idas
+for chave in voos.keys() :
+    if datas[chave].ano == comeca.ano and voos[chave].origem == plano.saida :
+        if (datas[chave].mes == comeca.mes and datas[chave].dia >= comeca.dia) or (datas[chave].mes > comeca.mes) :
+            if ((datas[chave].ano == acaba.ano and (datas[chave].mes < acaba.mes or (datas[chave].mes == acaba.mes and datas[chave].dia < acaba.dia)))
+                or datas[chave].ano < acaba.ano) :
+                possiveis_idas.append(chave)
+    elif datas[chave].ano < comeca.ano and voos[chave].origem == plano.saida :
+        possiveis_idas.append(chave)
 
-# se a data do vôo está dentro do período de férias de Jairzinho e se o destino do vôo é o mesmo aeroporto de onde Jairzinho sai,
-# adicionamos esse vôo na lista de possíveis voltas
-for i in range(len(voos)) :
-    if datas[i].ano == acaba.ano and voos[i].destino == plano.saida :
-        if (datas[i].mes == acaba.mes and datas[i].dia <= acaba.dia) or (datas[i].mes < acaba.mes) :
-            if ((datas[i].ano == comeca.ano and (datas[i].mes > comeca.mes or (datas[i].mes == comeca.mes and datas[i].dia > comeca.dia)))
-                  or datas[i].ano > comeca.ano) :
-                voos[i].indice = i
-                possiveis_voltas.append(voos[i])
-    elif datas[i].ano < acaba.ano and voos[i].destino == plano.saida :
-        voos[i].indice = i
-        possiveis_voltas.append(voos[i])
+# se a data do voo esta dentro do periodo de ferias de Jairzinho e se o destino do voo eh o mesmo aeroporto de onde Jairzinho sai,
+# adicionamos esse voo na lista de possiveis voltas
+for chave in voos.keys() :
+    if datas[chave].ano == acaba.ano and voos[chave].destino == plano.saida :
+        if (datas[chave].mes == acaba.mes and datas[chave].dia <= acaba.dia) or (datas[chave].mes < acaba.mes) :
+            if ((datas[chave].ano == comeca.ano and (datas[chave].mes > comeca.mes or (datas[chave].mes == comeca.mes and datas[chave].dia > comeca.dia)))
+                  or datas[chave].ano > comeca.ano) :
+                possiveis_voltas.append(chave)
+    elif datas[chave].ano < acaba.ano and voos[chave].destino == plano.saida :
+        possiveis_voltas.append(chave)
 
-# criação de uma lista que contém o par ida-volta de Jairzinho
+# criação de uma lista que contem o par ida-volta de Jairzinho
 par = []
 
 # selecionamos a viagem mais barata que tem no mínimo 4 dias
-# se há empate no valor, selecionamos a viagem de maior duração
+# se ha empate no valor, selecionamos a viagem de maior duracao
 for i in possiveis_idas :
     for j in possiveis_voltas :
-        if i.destino == j.origem and conta_dias(datas[i.indice], datas[j.indice]) >= 4 :
+        if voos[i].destino == voos[j].origem and conta_dias(datas[i], datas[j]) >= 4 :
             if len(par) == 0 :
                 par.append(i)
                 par.append(j)
-            elif ((i.valor + j.valor < par[0].valor + par[1].valor)
-                  or (i.valor + j.valor == par[0].valor + par[1].valor
-                      and conta_dias(datas[i.indice], datas[j.indice]) > conta_dias(datas[par[0].indice], datas[par[1].indice]))) :
+            elif ((voos[i].valor + voos[j].valor < voos[par[0]].valor + voos[par[1]].valor)
+                  or (voos[i].valor + voos[j].valor == voos[par[0]].valor + voos[par[1]].valor
+                      and conta_dias(datas[i], datas[j]) > conta_dias(datas[par[0]], datas[par[1]]))) :
                 par[0] = i
                 par[1] = j
 
-# o programa imprime os vôos de ida e de volta que atendem as necessidades de Jairzinho
-print (par[0].numero)
-print (par[1].numero)
+# o programa imprime os numeros dos de ida e de volta que atendem as necessidades de Jairzinho
+print (par[0])
+print (par[1])
